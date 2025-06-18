7558db684ce1f6513345806727ef6cb6190b074f
Nestor <nestorzamili@gmail.com>
2025-06-18 23:11:30 +0700

    chore: update middleware matcher to exclude image file types

--- src/middleware.ts
+++ src/middleware.ts
@@ -76,7 +76,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(png|jpg|jpeg|webp)$).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
